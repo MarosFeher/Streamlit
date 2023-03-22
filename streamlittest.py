dd20a9c17c5e288b0ec027f2aa6a28990fb9b3a5
MarosFeher <127991418+MarosFeher@users.noreply.github.com>
2023-03-22 21:59:03 +0100

    Add files via upload

---
+++ streamlittest.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+
+
+url = 'https://raw.githubusercontent.com/MarosFeher/Streamlit/main/top10medalistsCSV.csv'
+
+
+
+rows = pd.read_csv(url)
+
+# Print results.
+st.title(":orange[TOP 10 BEST MEDALISTS]")
+
+
+#### PANDAS Dataframe - jednoducha tabulka
+df = pd.DataFrame(rows)
+df.rename(columns={0: 'Athlete', 1: 'Sport', 2: 'Medals'}, inplace= True)
+df.index = pd.RangeIndex(start=1, stop=1+len(df), step=1)
+st.table(df)
+
+
+
