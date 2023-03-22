28e47a721578d39ad0674ca9e442a677b5597c33
MarosFeher <127991418+MarosFeher@users.noreply.github.com>
2023-03-22 22:40:58 +0100

    Update streamlittest.py

--- streamlittest.py
+++ streamlittest.py
@@ -1,16 +1,13 @@
 import streamlit as st
 import pandas as pd
 
-
-url = pd.read_csv('https://raw.githubusercontent.com/MarosFeher/Streamlit/main/top10medalistsCSV.csv', sep= ';')
-
-rows = pd.read_csv(url)
+rows = pd.read_csv('https://raw.githubusercontent.com/MarosFeher/Streamlit/main/top10medalistsCSV.csv', sep= ';')
 
 # Print results.
 st.title(":orange[TOP 10 BEST MEDALISTS]")
 
+print(rows)
 
-#### PANDAS Dataframe - jednoducha tabulka
 df = pd.DataFrame(rows)
 df.rename(columns={0: 'Athlete', 1: 'Sport', 2: 'Medals'}, inplace= True)
 df.index = pd.RangeIndex(start=1, stop=1+len(df), step=1)
