2a75d82700d8ae6c6419b8ea3eb68fc66409b574
MarosFeher <127991418+MarosFeher@users.noreply.github.com>
2023-03-25 22:55:58 +0100

    Update STREAMLIT.py

--- STREAMLIT.py
+++ STREAMLIT.py
@@ -1,6 +1,30 @@
 import streamlit as st
 import pandas as pd
 
+def TOP10():
+    import streamlit as st
+    import pandas as pd
+
+    rows = pd.read_csv('https://raw.githubusercontent.com/MarosFeher/Streamlit/main/top10medalistsCSV.csv', sep= ';')
+
+    # Print results.
+    st.title(":orange[TOP 10 BEST MEDALISTS]")
+
+    print(rows)
+
+    df = pd.DataFrame(rows)
+    df.columns = ['Athlete', 'Sport', 'Medals']
+    df.index = pd.RangeIndex(start=1, stop=1+len(df), step=1)
+    st.dataframe(df)
+
+
+page_names_to_funcs = {
+    "TOP 10 Medalists": TOP10
+}
+
+demo_name = st.sidebar.selectbox("Choose a demo", page_names_to_funcs.keys())
+page_names_to_funcs[demo_name]()
+
 st.set_page_config(
     page_title="Welcome",
     page_icon="👋",
