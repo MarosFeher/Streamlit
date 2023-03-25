725d2ad24e25f1b672dfd9b54250c59d4437db7f
MarosFeher <127991418+MarosFeher@users.noreply.github.com>
2023-03-25 22:40:05 +0100

    Update STREAMLIT.py

--- STREAMLIT.py
+++ STREAMLIT.py
@@ -1,6 +1,16 @@
 import streamlit as st
 import pandas as pd
 
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+st.write("# Welcome to Streamlit! 👋")
+
+st.sidebar.success("Select a demo above.")
+
+
 
 st.header('hello')
 st.write('https://marosfehertop10athletes.streamlit.app/')
