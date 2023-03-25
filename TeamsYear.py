67d19bcdd8fe8a4555986fa66261be3f4b6ab790
MarosFeher <127991418+MarosFeher@users.noreply.github.com>
2023-03-25 20:03:04 +0100

    Update TeamsYear.py

--- TeamsYear.py
+++ TeamsYear.py
@@ -16,10 +16,12 @@ st.header('Olympic Performance by Team/Country and Year :trophy:')
 select_team = st.multiselect('Select team or country: ', team)
 
 #pokial bude vybrana krajina, vytvori iba tabulku s danou krajinou. Pokial nebude tak vytvori celu tabulku
-while select_team:
-    for selected in select_team:
-        st.dataframe(table.loc[table['Olympic Team'].isin([selected])])
-        break
+if select_team:
+    try:
+        selected_rows = table[table['Olympic Team'].isin(select_team)]
+        st.dataframe(selected_rows)
+    except Exception as e:
+        st.error(f"Error occurred: {e}")
 else:
     st.dataframe(table)
 
