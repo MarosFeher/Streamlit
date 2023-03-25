32b8392c39db3ee15c3378c31a296d8a78e14c85
MarosFeher <127991418+MarosFeher@users.noreply.github.com>
2023-03-25 19:35:04 +0100

    Update TeamsYear.py

--- TeamsYear.py
+++ TeamsYear.py
@@ -19,7 +19,7 @@ select_team = st.multiselect('Select team or country: ', team)
 while select_team:
     try:
         for selected in select_team:
-            st.dataframe(table.loc[table['Olympic Team'].isin([selected])])
+            st.dataframe(table.iloc[table['Olympic Team'].isin([selected])])
         break
     except:
         break
